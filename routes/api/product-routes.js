ab3c230a1fd1b17d07df95573992676e81949f76
Sonika <sonikapoudyal5@gmail.com>
2021-07-11 17:45:40 -0400

    added function to the routes folder

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({}).then(dbProduct => {
+    res.json(dbProduct);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+  }).then(dbProduct => {
+    res.json(dbProduct);
+  })
 });
 
 // create new product
@@ -25,8 +35,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create(
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -91,6 +106,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbProduct => {
+    res.json(dbProduct);
+  })
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,54 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({}).then(dbTag => {
+    res.json(dbTag);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbTag => {
+    res.json(dbTag);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(
+    {
+      tag_name: req.body.tag_name
+    }
+  ).then(dbTag => {
+    res.json(dbTag);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbTag => {
+    res.json(dbTag);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbTag => {
+    res.json(dbTag);
+  });
 });
 
 module.exports = router;
